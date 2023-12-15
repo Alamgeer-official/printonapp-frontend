1fefa198adfe38ed486430e201bb4ca299b9e86f
Alamgeer <alamgeer.official.id@gmail.com>
2023-12-15 22:46:32 +0530

    template setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Navbar } from "./component/Navbar";
+import { Footer } from "./component/Footer";
+import { Signup } from "./component/Signup";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      <Navbar></Navbar>
+      <Signup></Signup>
+      <Footer></Footer>
     </>
   );
 }
